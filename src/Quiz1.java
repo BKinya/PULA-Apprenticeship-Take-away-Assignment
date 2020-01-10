ecf898349ea0e22308869e2f9480957a85fe3ae8
BKinya <gitongakinya1@gmail.com>
2020-01-10 19:03:34 +0300

    Updated Question 1

--- src/Quiz1.java
+++ src/Quiz1.java
@@ -18,15 +18,17 @@ public class Quiz1 {
 //
             System.out.println("Enter your age");
             int age = mScanner.nextInt();
-            System.out.println(name + " in " + year + " your age is " + age + " years");
-
             //check if age is a multiple of 7
-            // if it's not increment until you get the nearest multiple of 7
+            // if not print current age
+            // increment until you get the nearest multiple of 7
+            if (age % 7 != 0){
+                System.out.println(name + " in " + year + " your age is " + age + " years");
+            }
             while (age % 7 != 0) {
                 ++age;
                 ++year;
             }
-            //print age in multiples of 7 until it is 100 years
+            // then print age in multiples of 7 until 100 years
             incrementAge(name, age, year);
 
         } catch (InputMismatchException e) {
@@ -39,8 +41,10 @@ public class Quiz1 {
     /**
      * @param age
      * @param year
+     *
+     * print age in multiples of 7 until the 100 years
      */
-    public static void incrementAge(String name, int age, int year) {
+    private static void incrementAge(String name, int age, int year) {
         do {
             System.out.println(name + " in " + year + " your age is " + age + " years");
             year += 7;
